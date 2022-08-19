d1ca3bf80a703bcf8d9bd826bf9969f61c124af8
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-08-19 18:52:15 +0300

    +

---
+++ src/js/02-timer.js
@@ -0,0 +1,23 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+};
+
+refs.btnStart.toggleAttribute('disabled');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= options.defaultDate) {
+      window.alert('Please choose a date in the future');
+    }
+    refs.btnStart.toggleAttribute('disabled');
+  },
+};
+
+flatpickr('[id="datetime-picker"]', options);
