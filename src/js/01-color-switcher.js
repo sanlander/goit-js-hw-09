1f0481fbc1b50dc5445902ceb15a323951d5dde2
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-08-19 18:18:32 +0300

    hw-1 +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+refs.btnStop.toggleAttribute('disabled');
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function changeBodyBgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onBtnStart(e) {
+  timerId = setInterval(changeBodyBgColor, 1000);
+  refs.btnStart.toggleAttribute('disabled');
+    refs.btnStop.toggleAttribute('disabled');
+    console.log(timerId);
+}
+
+function onBtnStop() {
+  clearInterval(timerId);
+  refs.btnStart.toggleAttribute('disabled');
+    refs.btnStop.toggleAttribute('disabled');
+    console.log(timerId);
+}
